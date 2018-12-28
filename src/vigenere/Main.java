6edf20eebe6c6de29be04d0b0c0008a4b0b8af63
IGNovikov <IGNovikov@esphere.ru>
2018-12-28 18:20:02 +0300

    + текст Lorem ipsum

--- src/vigenere/Main.java
+++ src/vigenere/Main.java
@@ -1,10 +1,16 @@
 package vigenere;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         String[] abcEN = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
         String[] abcRU = {"а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"};
 
+        String[] arr = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore " +
+                "et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip " +
+                "ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu " +
+                "fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.").toLowerCase().split("");
     }
 }
