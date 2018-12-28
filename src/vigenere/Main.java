905500d3f5560dc94686d8d3a9fc19da0872bb36
IGNovikov <IGNovikov@esphere.ru>
2018-12-28 17:43:54 +0300

    Добавил массив русский и английских символов

--- src/vigenere/Main.java
+++ src/vigenere/Main.java
@@ -3,5 +3,8 @@ package vigenere;
 public class Main {
     public static void main(String[] args) {
 
+        String[] abcEN = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        String[] abcRU = {"а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"};
+
     }
 }
