9555f1e30f756fda8adcfabf9a72aeca3ccad387
IGNovikov <IGNovikov@esphere.ru>
2019-01-10 09:50:50 +0300

    added package Warmup rename classes refactor

--- src/codingbat/Warmup1/MonkeyTrouble.java
+++ src/codingbat/Warmup1/MonkeyTrouble.java
@@ -1,6 +1,6 @@
-package codingbat;
+package codingbat.Warmup1;
 
-public class Warmup02_monkeyTrouble {
+public class MonkeyTrouble {
 
     //We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
     //We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
--- src/codingbat/Warmup1/SleepIn.java
+++ src/codingbat/Warmup1/SleepIn.java
@@ -1,6 +1,6 @@
-package codingbat;
+package codingbat.Warmup1;
 
-public class Warmup01_SleepIn {
+public class SleepIn {
 
     // The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
     // We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
